0306c74e19a98431fae7fb4f52633b2717fb9135
pjordaan <pjordaan@users.noreply.github.com>
2023-09-10 15:04:38 +0000

    Update file(s) "packages/laravel-apie/." from "apie-lib/apie-lib-monorepo"

--- resources/apie.php
+++ resources/apie.php
@@ -5,6 +5,8 @@ use Apie\Cms\RouteDefinitions\CmsRouteDefinitionProvider;
 use Apie\CmsApiDropdownOption\RouteDefinitions\DropdownOptionsForExistingObjectRouteDefinition;
 use Apie\Common\Wrappers\RequestAwareInMemoryDatalayer;
 use Apie\Console\ConsoleCommandFactory;
+use Apie\DoctrineEntityConverter\EntityBuilder;
+use Apie\DoctrineEntityDatalayer\DoctrineEntityDatalayer;
 use Apie\Faker\ApieObjectFaker;
 use Apie\RestApi\OpenApi\OpenApiGenerator;
 
@@ -31,6 +33,11 @@ return [
             //]
         ]
     ],
+    'doctrine' => [
+        'build_once' => false,
+        'run_migrations' => false,
+        'connection_params' => [],
+    ],
     'bounded_contexts' => [
         'default' => [
             'entities_folder' => app_path('Apie/Entities'),
@@ -42,6 +49,9 @@ return [
     'enable_core' => true,
     'enable_cms' => class_exists(CmsRouteDefinitionProvider::class),
     'enable_cms_dropdown' => class_exists(DropdownOptionsForExistingObjectRouteDefinition::class),
+    'enable_doctrine_entity_converter' => class_exists(EntityBuilder::class),
+    'enable_doctrine_entity_datalayer' => class_exists(DoctrineEntityDatalayer::class),
+    /* 'enable_doctrine_bundle_connection'  symfony only*/
     'enable_faker' => class_exists(ApieObjectFaker::class),
     'enable_rest_api' => class_exists(OpenApiGenerator::class),
     'enable_console' => class_exists(ConsoleCommandFactory::class),
